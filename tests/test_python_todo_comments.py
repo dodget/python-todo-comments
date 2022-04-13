958e3cbeeaa8040e69a7ff66847af0d4a0618896
Tylor Dodge <tdodge@nexamp.com>
2022-04-13 11:22:33 -0400

    Add tox

--- tests/test_python_todo_comments.py
+++ tests/test_python_todo_comments.py
@@ -1,5 +1,16 @@
 from python_todo_comments import __version__
+from python_todo_comments.main import main
+
+# TODO: test comment
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0'
+
+
+def test_main(capsys):
+    main(args=["--dir", "./tests/"])
+    captured = capsys.readouterr()
+    print("HERE")
+    print(captured.out)
+    assert captured.out == "### tests/test_python_todo_comments.py\n- 4: test comment\n\n"
